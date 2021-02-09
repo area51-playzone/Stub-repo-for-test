cd673ea9d766325aab7fc5917525f143073a2e0b
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2021-02-09 13:15:30 -0800

    rmv methods

--- src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
+++ src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
@@ -18,16 +18,4 @@ public class CalculatorTest {
 		Assert.assertEquals(8, iby4);
 	}
 
-	public void testMinus() {
-		Calculator calculator = new Calculator();
-		int result = calculator.subtract(10, 5);
-		Assert.assertEquals(5, result);
-		// When
-		float a = 5.0;
-		float b = 6.0;
-		result = calculator.sum(a, b);
-		int iby4 = calculator.incrementBy4(result);
-		calculator.sum(result, 5);
-		Assert.assertTrue(result == 5);
-	}
 }
