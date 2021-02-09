8e6a9db2fd5fa27a26c76f72e26286b742bc4e80
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2021-02-09 15:12:46 -0800

    Remove all src methods

--- src/main/java/com/github/stokito/unitTestExample/calculator/Calculator.java
+++ src/main/java/com/github/stokito/unitTestExample/calculator/Calculator.java
@@ -5,18 +5,4 @@ public class Calculator {
 	public int sum(int a, int b) {
 		return a + b;
 	}
-	public void sum (int a, int b) {
-		System.out.println(a + b);
-	}
-
-	public int incrementBy4(int a) {
-		return a + 4;
-	}
-
-	public int subtract(int a , int b) {
-		return a - b ;
-	}
-	public int sum(float a, float b) {
-		return (int) (a + b);
-	}
 }
